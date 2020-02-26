2b886ea1377850d18ece5555c72b6b11a0c81158
James <James@144.37.70.234>
2020-02-25 19:50:56 -0800

    razzle dazzle

--- src/main/java/lab4/Client.java
+++ src/main/java/lab4/Client.java
@@ -1,9 +1,13 @@
 package lab4;
 
 public class Client{
-    //WeaponIF WeaponPanel;
-    //CharacterIF CharacterPanel;
-    public static void main(String[] args){
-        System.out.print("Hello");
+  
+    WeaponIF WeaponPanel;
+    CharacterIF CharacterPanel;
+    
+    public static void main(String[] args) {
+        //Utility factory = new Utility();
+        //factory.createFactory("ADVANCED");
+        System.out.println("Success!");
     } 
 }
